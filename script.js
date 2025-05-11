3b0f264fdea5a93c0fd7b9fff58e08ac40fe0660
Falrafa4 <rafaasad9@gmail.com>
2025-05-11 10:39:47 +0700

    menambah style BMI & BMR, logika BMI dioptimisasi, menambah gelombang, mengubah direktori image ke assets, hapus bootstrap

--- script.js
+++ script.js
@@ -26,6 +26,12 @@ function nonaktif(id) {
   if (id) {
     element.style.display = 'none';
   }
+
+  document.querySelectorAll("input[type='number']").forEach(input => {
+    input.value = "";
+  });
+
+  document.getElementById('output').style.display = 'none';
 }
 
 // untuk membatasi input
@@ -51,9 +57,13 @@ function outputBmi(id) {
   bmi = bmi.toFixed(2);
   if (bmi <= 18.5) {
     document.querySelector('.output-text').innerHTML = "Berdasarkan perhitungan, Indeks Massa Tubuh kamu sebesar <b>" + bmi + "</b>! Ini berarti kamu termasuk kategori underweight (berat badan kurang). Maka kamu harus surplus atau memperbanyak kalori harian kamu lebih dari biasanya!";
-  } else if (bmi <= 25 && bmi > 18.5) {
+  } else if (bmi <= 22.9 && bmi > 18.5) {
     document.querySelector('.output-text').innerHTML = "Berdasarkan perhitungan, Indeks Massa Tubuh kamu sebesar <b>" + bmi + "</b>! Ini berarti kamu termasuk kategori ideal (berat badan ideal). Maka kamu harus mempertahankannya agar tidak kegemukan atau kekurusan!";
-  } else if (bmi > 25) {
+  } else if (bmi <= 24.9 && bmi > 23) {
     document.querySelector('.output-text').innerHTML = "Berdasarkan perhitungan, Indeks Massa Tubuh kamu sebesar <b>" + bmi + "</b>! Ini berarti kamu termasuk kategori overweight (berat badan berlebih). Maka kamu harus melakukan defisit kalori atau mengurangi asupan kalori kamu lebih dari biasanya!";
+  } else if (bmi > 25) {
+    document.querySelector('.output-text').innerHTML = "Berdasarkan perhitungan, Indeks Massa Tubuh kamu sebesar <b>" + bmi + "</b>! Wah gawat, kamu termasuk kategori obesitas. Maka kamu harus melakukan defisit kalori besar-besaran serta melakukan diet untuk mengurangi berat badanmu!";
+  } else {
+    document.querySelector('.output-text').innerHTML = 'Invalid input!';
   }
 }
